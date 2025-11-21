e34a63dcdadb6deec6e083e2534b597335319d66
daury <DRVR0001@ce.pucmm.edu.do>
2025-11-21 18:57:58 -0400

    Logica Registrar Equipo

--- src/main/java/com/example/base_de_datos/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.base_de_datos;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(PaginaPrincipal.class, args);
-    }
-}
---
+++ src/main/java/com/example/base_de_datos/Logico/CiudadItem.java
@@ -0,0 +1,24 @@
+package com.example.base_de_datos.Logico;
+
+public class CiudadItem {
+    private String id;
+    private String nombre;
+
+    public CiudadItem(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
